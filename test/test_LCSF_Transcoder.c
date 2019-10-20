a6dddab9e8b98882a8d679cb7ccfd11487f61c7f
Jean-Roland <jean.roland.gosse@gmail.com>
2019-10-20 15:44:24 +0200

    test: add transcoder and validator unit test

---
+++ test/test_LCSF_Transcoder.c
@@ -0,0 +1,232 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "unity.h"
+
+#include "LCSF_Transcoder.h"
+#include "Filo.h"
+
+#include "mock_MemAlloc.h"
+#include "mock_LCSF_Validator.h"
+
+// *** Constants ***
+#define FILO_SIZE 10
+#define BUFF_SIZE 255
+
+// *** Private functions prototypes
+static bool compare_rawatt(const lcsf_raw_att_t *pAtt1, const lcsf_raw_att_t *pAtt2);
+static bool compare_rawmsg(const lcsf_raw_msg_t *pMsg1, const lcsf_raw_msg_t *pMsg2);
+static void *calloc_Callback(uint32_t size, int num_calls);
+static void *malloc_Callback(uint32_t size, int num_calls);
+static bool process_error_Callback(uint8_t error_code, int num_calls);
+static bool process_msg_Callback(const lcsf_raw_msg_t *pMsg, int num_calls);
+static bool send_Callback(const uint8_t *pBuffer, uint16_t buffSize);
+
+// *** Descriptors ***
+static const lcsf_trnscdr_init_desc_t init_trnscdr = {
+    send_Callback,
+    FILO_SIZE,
+    BUFF_SIZE,
+};
+
+// *** Private global vars ***
+static void *memPtr[64];
+static int memIdx;
+
+// *** Model data ***
+static const uint8_t ovrflwMsg[] = {0xaa, 0x00, 0x01, 0x00, 0x0b, 0x00};
+
+static const uint8_t badformatMsg[] = {0xaa, 0x00, 0x01, 0x00, 0x0a, 0x00};
+
+static const uint8_t rxMsg[] = {
+0xab, 0x00, 0x00, 0x12, 0x03, 0x00, 0x55, 0x00, 0x05, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04,
+0xff, 0xff, 0x02, 0x00, 0x30, 0x00, 0x01, 0x00, 0x0a, 0x31, 0x80, 0x01, 0x00, 0x32, 0x00,
+0x02, 0x00, 0xab, 0xcd, 0x40, 0x00, 0x10, 0x00, 0x4f, 0x72, 0x67, 0x61, 0x6e, 0x6f, 0x6c,
+0x65, 0x70, 0x74, 0x69, 0x63, 0x00, 0x00, 0x00, 0x00
+};
+
+// Model raw msg
+static uint8_t txAtt1Data[] = {0x00, 0x01, 0x02, 0x03, 0x04};
+
+static uint8_t txAtt2Data[] = {0xa};
+
+static uint8_t txAtt3Data[] = {0xab, 0xcd};
+
+static uint8_t txAtt4Data[] = { // Organoleptic
+0x4f, 0x72, 0x67, 0x61, 0x6e, 0x6f, 0x6c, 0x65,
+0x70, 0x74, 0x69, 0x63, 0x00, 0x00, 0x00, 0x00
+};
+
+static lcsf_raw_att_t txSubSubAtt[] = {
+    {
+        0x32,
+        false,
+        sizeof(txAtt3Data),
+        {
+            .pData = txAtt3Data,
+        }
+    }
+};
+
+static lcsf_raw_att_t txSubAttArr[] = {
+    {
+        0x30,
+        false,
+        sizeof(txAtt2Data),
+        {
+            .pData = txAtt2Data,
+        }
+    },
+    {
+        0x31,
+        true,
+        sizeof(txSubSubAtt)/sizeof(lcsf_raw_att_t),
+        {
+            .pSubAttArray = txSubSubAtt,
+        }
+    },
+};
+
+static lcsf_raw_att_t txAttArr[] = {
+    {
+        0x55,
+        false,
+        sizeof(txAtt1Data),
+        {
+            .pData = txAtt1Data,
+        }
+    },
+    {
+        0x7fff,
+        true,
+        sizeof(txSubAttArr)/sizeof(lcsf_raw_att_t),
+        {
+            .pSubAttArray = txSubAttArr,
+        }
+    },
+    {
+        0x40,
+        false,
+        sizeof(txAtt4Data),
+        {
+            .pData = txAtt4Data,
+        }
+    },
+};
+
+static const lcsf_raw_msg_t txMsg = {
+    0xab,
+    0x1200,
+    sizeof(txAttArr)/sizeof(lcsf_raw_att_t),
+    txAttArr
+};
+
+// *** Private Functions ***
+static bool compare_rawatt(const lcsf_raw_att_t *pAtt1, const lcsf_raw_att_t *pAtt2) {
+    if (pAtt1->AttId != pAtt2->AttId) {
+        return false;
+    }
+    if (pAtt1->HasSubAtt != pAtt2->HasSubAtt) {
+        return false;
+    }
+    if (pAtt1->DataSize != pAtt2->DataSize) {
+        return false;
+    }
+    if (pAtt1->HasSubAtt) {
+        for (uint16_t idx = 0; idx < pAtt1->DataSize; idx++) {
+            if (!compare_rawatt(&pAtt1->Payload.pSubAttArray[idx], &pAtt2->Payload.pSubAttArray[idx])) {
+                return false;
+            }
+        }
+    } else {
+        if (memcmp(pAtt1->Payload.pData, pAtt2->Payload.pData, pAtt1->DataSize) != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool compare_rawmsg(const lcsf_raw_msg_t *pMsg1, const lcsf_raw_msg_t *pMsg2) {
+    if (pMsg1->ProtId != pMsg2->ProtId) {
+        return false;
+    }
+    if (pMsg1->CmdId != pMsg2->CmdId) {
+        return false;
+    }
+    if (pMsg1->AttNb != pMsg2->AttNb) {
+        return false;
+    }
+    for (uint16_t idx = 0; idx < pMsg1->AttNb; idx++) {
+        if (!compare_rawatt(&pMsg1->pAttArray[idx], &pMsg2->pAttArray[idx])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void *calloc_Callback(uint32_t size, int num_calls) {
+    memPtr[memIdx] = calloc(size,1);
+    return memPtr[memIdx++];
+}
+
+static void *malloc_Callback(uint32_t size, int num_calls) {
+    memPtr[memIdx] = malloc(size);
+    return memPtr[memIdx++];
+}
+
+static bool process_error_Callback(uint8_t error_code, int num_calls) {
+    switch (num_calls) {
+        case 0:
+            return (error_code == 0xFF);
+        case 1:
+            return (error_code == 0x01);
+        case 2:
+            return (error_code == 0x00);
+        default:
+            return false;
+    }
+}
+
+static bool process_msg_Callback(const lcsf_raw_msg_t *pMsg, int num_calls) {
+    return compare_rawmsg(pMsg, &txMsg);
+}
+
+static bool send_Callback(const uint8_t *pBuffer, uint16_t buffSize) {
+    return (memcmp(pBuffer, rxMsg, buffSize) == 0);
+}
+
+// *** Public Functions ***
+void setUp(void) {
+    // Declare callback
+    MemAllocCalloc_StubWithCallback(calloc_Callback);
+    MemAllocMalloc_StubWithCallback(malloc_Callback);
+    LCSF_ValidatorSendTranscoderError_StubWithCallback(process_error_Callback);
+    LCSF_ValidatorReceive_StubWithCallback(process_msg_Callback);
+    // Test init module
+    TEST_ASSERT_FALSE(LCSF_TranscoderInit(NULL));
+    TEST_ASSERT_TRUE(LCSF_TranscoderInit(&init_trnscdr));
+}
+
+void tearDown(void) {
+    // Free allocated memory
+    for (int idx = 0; idx < memIdx; idx++) {
+        free(memPtr[idx]);
+    }
+    memIdx = 0;
+}
+
+void test_LCSF_Transcoder_Encode(void) {
+    // Test error cases
+    TEST_ASSERT_FALSE(LCSF_TranscoderSend(NULL));
+    // Test valid message
+    TEST_ASSERT_TRUE(LCSF_TranscoderSend(&txMsg));
+}
+
+void test_LCSF_Transcoder_Decode(void) {
+    // Test error cases
+    TEST_ASSERT_TRUE(LCSF_TranscoderReceive(NULL, 0));
+    TEST_ASSERT_TRUE(LCSF_TranscoderReceive(ovrflwMsg, sizeof(ovrflwMsg)));
+    TEST_ASSERT_TRUE(LCSF_TranscoderReceive(badformatMsg, sizeof(badformatMsg)));
+    // Test valid packet
+    TEST_ASSERT_TRUE(LCSF_TranscoderReceive(rxMsg, sizeof(rxMsg)));
+}
---
+++ test/test_LCSF_Validator.c
@@ -0,0 +1,231 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "unity.h"
+
+#include "LCSF_Validator.h"
+#include "Filo.h"
+#include "LCSF_Desc_Example.h"
+#include "LCSF_Bridge_Example.h"
+#include "Example_Main.h"
+
+#include "mock_MemAlloc.h"
+#include "mock_LCSF_Transcoder.h"
+
+// *** Constants ***
+#define FILO_SIZE 10
+#define PROTOCOL_NB 1
+#define ERROR_CODE 0xff
+
+// *** Private functions prototypes
+static void *calloc_Callback(uint32_t size, int num_calls);
+static void *malloc_Callback(uint32_t size, int num_calls);
+static bool process_error_Callback(uint8_t error_code, int num_calls);
+static bool process_Callback(const lcsf_raw_msg_t *pMsg, int num_calls);
+static bool interpret_Callback(lcsf_valid_cmd_t *pValidCmd);
+
+// *** Descriptors ***
+static const lcsf_validator_init_desc_t init_vldtr = {
+    FILO_SIZE,
+    PROTOCOL_NB,
+};
+
+static const lcsf_validator_protocol_desc_t example_prot_desc = {
+    LCSF_EXAMPLE_PROTOCOL_ID,
+    &LCSF_Example_ProtDesc,
+    interpret_Callback,
+};
+
+// *** Private global vars ***
+static void *memPtr[64];
+static int memIdx;
+
+// *** Model data ***
+
+// Model error msg
+static uint8_t errloc = 0x00;
+static uint8_t errcode = ERROR_CODE;
+
+static lcsf_raw_att_t errAttArr[] = {
+    {
+        0x00,
+        false,
+        sizeof(uint8_t),
+        {
+            .pData = &errloc,
+        }
+    },
+    {
+        0x01,
+        false,
+        sizeof(uint8_t),
+        {
+            .pData = &errcode,
+        }
+    },
+};
+
+static const lcsf_raw_msg_t errMsg = {
+    0xFFFF,
+    0x00,
+    2,
+    errAttArr,
+};
+
+// Attribute data
+static uint8_t att1Data[] = {0x04};
+
+// Model raw msg
+static lcsf_raw_att_t rxAttArr[] = {
+    {
+        LCSF_EXAMPLE_ATT_ERROR_CODE,
+        false,
+        sizeof(att1Data),
+        {
+            .pData = att1Data,
+        }
+    },
+};
+
+static const lcsf_raw_msg_t rxMsg = {
+    LCSF_EXAMPLE_PROTOCOL_ID,
+    LCSF_EXAMPLE_CMD_ID_ERROR,
+    LCSF_EXAMPLE_CMD_ERROR_ATT_NB,
+    rxAttArr,
+};
+
+
+// Model valid msg
+static lcsf_valid_att_t txAttArr[] = {
+    sizeof(att1Data),
+    {
+        .pData = att1Data,
+    }
+};
+
+static const lcsf_valid_cmd_t txMsg = {
+    LCSF_EXAMPLE_CMD_ID_ERROR,
+    txAttArr,
+};
+
+// *** Private Functions ***
+static bool compare_validatt(const lcsf_valid_att_t *pAtt1, const lcsf_valid_att_t *pAtt2) {
+    if (pAtt1->PayloadSize != pAtt2->PayloadSize) {
+        return false;
+    }
+    if (memcmp(pAtt1->Payload.pData, pAtt2->Payload.pData, pAtt1->PayloadSize) != 0) {
+        return false;
+    }
+    return true;
+}
+
+static bool compare_validmsg(const lcsf_valid_cmd_t *pMsg1, const lcsf_valid_cmd_t *pMsg2) {
+    if (pMsg1->CmdId != pMsg2->CmdId) {
+        return false;
+    }
+    for (uint16_t idx = 0; idx < LCSF_EXAMPLE_CMD_ERROR_ATT_NB; idx++) {
+        if (!compare_validatt(&pMsg1->pAttArray[idx], &pMsg2->pAttArray[idx])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool compare_rawatt(const lcsf_raw_att_t *pAtt1, const lcsf_raw_att_t *pAtt2) {
+    if (pAtt1->AttId != pAtt2->AttId) {
+        return false;
+    }
+    if (pAtt1->HasSubAtt != pAtt2->HasSubAtt) {
+        return false;
+    }
+    if (pAtt1->DataSize != pAtt2->DataSize) {
+        return false;
+    }
+    if (pAtt1->HasSubAtt) {
+        for (uint16_t idx = 0; idx < pAtt1->DataSize; idx++) {
+            if (!compare_rawatt(&pAtt1->Payload.pSubAttArray[idx], &pAtt2->Payload.pSubAttArray[idx])) {
+                return false;
+            }
+        }
+    } else {
+        if (memcmp(pAtt1->Payload.pData, pAtt2->Payload.pData, pAtt1->DataSize) != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool compare_rawmsg(const lcsf_raw_msg_t *pMsg1, const lcsf_raw_msg_t *pMsg2) {
+    if (pMsg1->ProtId != pMsg2->ProtId) {
+        return false;
+    }
+    if (pMsg1->CmdId != pMsg2->CmdId) {
+        return false;
+    }
+    if (pMsg1->AttNb != pMsg2->AttNb) {
+        return false;
+    }
+    for (uint16_t idx = 0; idx < pMsg1->AttNb; idx++) {
+        if (!compare_rawatt(&pMsg1->pAttArray[idx], &pMsg2->pAttArray[idx])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void *calloc_Callback(uint32_t size, int num_calls) {
+    memPtr[memIdx] = calloc(size,1);
+    return memPtr[memIdx++];
+}
+
+static void *malloc_Callback(uint32_t size, int num_calls) {
+    memPtr[memIdx] = malloc(size);
+    return memPtr[memIdx++];
+}
+
+static bool process_Callback(const lcsf_raw_msg_t *pMsg, int num_calls) {
+    switch (num_calls) {
+        case 0:
+            return compare_rawmsg(pMsg, &errMsg);
+        case 1:
+            return compare_rawmsg(pMsg, &rxMsg);
+        default:
+            return false;
+    }
+}
+
+static bool interpret_Callback(lcsf_valid_cmd_t *pValidCmd) {
+    return compare_validmsg(pValidCmd, &txMsg);
+}
+
+// *** Public Functions ***
+void setUp(void) {
+    // Set callbacks
+    MemAllocCalloc_StubWithCallback(calloc_Callback);
+    MemAllocMalloc_StubWithCallback(malloc_Callback);
+    LCSF_TranscoderSend_StubWithCallback(process_Callback);
+    // Test init module
+    TEST_ASSERT_FALSE(LCSF_ValidatorInit(NULL));
+    TEST_ASSERT_TRUE(LCSF_ValidatorInit(&init_vldtr));
+    TEST_ASSERT_FALSE(LCSF_ValidatorAddProtocol(0, NULL));
+    TEST_ASSERT_TRUE(LCSF_ValidatorAddProtocol(0, &example_prot_desc));
+}
+
+void tearDown(void) {
+    // Free allocated memory
+    for (int idx = 0; idx < memIdx; idx++) {
+        free(memPtr[idx]);
+    }
+    memIdx = 0;
+}
+
+void test_LCSF_Validator_Receive(void) {
+    TEST_ASSERT_FALSE(LCSF_ValidatorReceive(NULL));
+    TEST_ASSERT_TRUE(LCSF_ValidatorReceive(&rxMsg));
+}
+
+void test_LCSF_Validator_Send(void) {
+    TEST_ASSERT_TRUE(LCSF_ValidatorSendTranscoderError(ERROR_CODE));
+    TEST_ASSERT_FALSE(LCSF_ValidatorSend(LCSF_EXAMPLE_PROTOCOL_ID, NULL));
+    TEST_ASSERT_TRUE(LCSF_ValidatorSend(LCSF_EXAMPLE_PROTOCOL_ID, &txMsg));
+}
